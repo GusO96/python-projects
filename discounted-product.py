0b210e9e8c155c494580d91275e115b09c9a07a9
GusO96 <gustavoliveira96@hotmail.com>
2024-02-29 01:37:18 -0300

    adding new project

---
+++ discounted-product.py
@@ -0,0 +1,22 @@
+print()
+print('DISCOUNTED PRODUCT')
+print()
+
+# Receiving the name and value of the product
+product_name = (input('Enter the product name: '))
+value = (float(input('Enter the value of the product: ')))
+
+# Discount calculation
+discount = value * 0.12
+total = value - discount
+
+# Print the result
+print()
+print('COUPON')
+print()
+print(f'Product: {product_name}')
+print(f'Value: {value:.2f} $')
+print(f'Discount: {discount} $')
+print()
+print(f'Total: {total} $')
+print()
