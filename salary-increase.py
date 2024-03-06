85f228ea5eb6190575073bb01f1749f1bffc6011
GusO96 <gustavoliveira96@hotmail.com>
2024-03-05 23:28:29 -0300

    update1

---
+++ salary-increase.py
@@ -0,0 +1,19 @@
+print()
+print('SALARY INCREASE')
+print()
+
+# Receiving the employee's name and salary
+name = (input('Enter your name: '))
+salary = (float(input('Enter your salary: ')))
+
+# Calculation of salary increase
+increase = salary * 0.25
+total = salary + increase
+
+# Printing the result
+print()
+print('INFORMATION - 25% INCREASE')
+print(f'Name: {name}')
+print(f'Salary: {salary:.2f} $')
+print(f'Increase: {increase:.2f} $')
+print(f'New Salary: {total:.2f} $')
