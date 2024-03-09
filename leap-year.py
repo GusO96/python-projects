a9bbbb461c2e64d39e333f4914e02a371d729d9d
GusO96 <gustavoliveira96@hotmail.com>
2024-03-09 15:04:22 -0300

    update7

---
+++ leap-year.py
@@ -0,0 +1,13 @@
+print()
+print('LEAP YEAR')
+print()
+
+year = int(input('Enter the year: '))
+print()
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print(f'The year {year} is a leap year.')
+    print()
+else:
+    print(f'The year {year} is not a leap year')
+    print()
